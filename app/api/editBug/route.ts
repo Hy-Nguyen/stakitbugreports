0a3d74fc6a94ad5848ab89c82ae11d86ad79884a
Hy Nguyen <giahy316@gmail.com>
2024-11-20 20:13:22 -0700

    update fix

--- app/api/editBug/route.ts
+++ app/api/editBug/route.ts
@@ -2,13 +2,13 @@ import { createClient } from '@/lib/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   const body = await req.json();
+  const { image_count, ...updateData } = body;
 
   const supabase = await createClient();
-  const { error } = await supabase.from('stakit_bugs').update(body).eq('id', body.id);
+  const { error } = await supabase.from('stakit_bugs').update(updateData).eq('id', body.id);
   console.log(error);
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
   return NextResponse.json({ message: 'Bug updated successfully' }, { status: 200 });
 }
-
